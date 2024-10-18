dd3c0b6c5f15216d0242d7bf1bce3afebb7c48af
thomascrecy <126467625+thomascrecy@users.noreply.github.com>
2024-10-18 10:51:26 +0200

    valid

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -9,27 +9,15 @@ s.send('Hello'.encode())
 data = s.recv(1024)
 
 def is_calcul(value: str):
-    # Utilisation de re pour capturer les deux nombres et l'opérateur
-    match = re.search(r'^(-?\d+)\s*([\+\-\*])\s*(-?\d+)$', value)
-    if match:
-        num1 = int(match.group(1))
-        num2 = int(match.group(3))
-        return num1, num2, match.group(2)  # On retourne les deux nombres et l'opérateur
-    return None
+    return re.search(r'^(-?\d+)\s*[\+\-\*]\s*(-?\d+)$', value)
 
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
-result = is_calcul(msg)
-
-if not result:
+if not is_calcul(msg):
     raise ValueError("Ceci n'est pas un calcul")
 
-num1, num2, operator = result
-
-# Vérif si les nombres sont compris entre -1048575 et +1048575
-min_value, max_value = -1048575, 1048575
-if not (min_value <= num1 <= max_value and min_value <= num2 <= max_value):
-    raise ValueError("Les nombres doivent être compris entre -1048575 et +1048575")
+# Verif si les nombres sont compris entre -1048575 et +1048575
+# print("ERROR: Veuillez entrer des nombres entre -1048575 et +1048575.")
 
 # on encode le message explicitement en UTF-8 pour récup un tableau de bytes
 encoded_msg = msg.encode('utf-8')
