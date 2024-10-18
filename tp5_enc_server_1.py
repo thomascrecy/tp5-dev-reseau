ca4594bcda2bb0e3fe264343120209f3cf163101
thomascrecy <126467625+thomascrecy@users.noreply.github.com>
2024-10-18 11:01:45 +0200

    server

--- tp5_enc_server_1.py
+++ tp5_enc_server_1.py
@@ -8,24 +8,37 @@ s.listen(1)
 conn, addr = s.accept()
 
 while True:
-
-    try:
-        # On reçoit la string Hello du client
-        data = conn.recv(1024)
-        if not data: break
-        print(f"Données reçues du client : {data}")
-
-        conn.send("Hello".encode())
-
-        # On reçoit le calcul du client
-        data = conn.recv(1024)
-
-        # Evaluation et envoi du résultat
-        res  = eval(data.decode())
-        conn.send(str(res).encode())
-         
-    except socket.error:
-        print("Error Occured.")
+    # On lit les 4 premiers octets qui arrivent du client
+    # Car dans le client, on a fixé la taille du header à 4 octets
+    header = conn.recv(4)
+    if not header:
         break
 
+    # On lit la valeur
+    msg_len = int.from_bytes(header[0:4], byteorder='big')
+
+    print(f"Lecture des {msg_len} prochains octets")
+
+    # Une liste qui va contenir les données reçues
+    chunks = []
+
+    bytes_received = 0
+    while bytes_received < msg_len:
+        # Si on reçoit + que la taille annoncée, on lit 1024 par 1024 octets
+        chunk = conn.recv(min(msg_len - bytes_received,
+                                1024))
+        if not chunk:
+            raise RuntimeError('Invalid chunk received bro')
+
+        # on ajoute le morceau de 1024 ou moins à notre liste
+        chunks.append(chunk)
+
+        # on ajoute la quantité d'octets reçus au compteur
+        bytes_received += len(chunk)
+
+    # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
+    message_received = b"".join(chunks).decode('utf-8')
+    print(f"Received from client {message_received}")
+
 conn.close()
+s.close()
