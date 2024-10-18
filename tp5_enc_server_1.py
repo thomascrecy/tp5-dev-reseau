26ae6e2525e3254ba55c4557f9cd4613afe1b913
thomascrecy <126467625+thomascrecy@users.noreply.github.com>
2024-10-18 10:08:05 +0200

    l

--- tp5_enc_server_1.py
+++ tp5_enc_server_1.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('127.0.0.1', 9999))  
+s.bind(('10.4.4.11', 13337))  
 
 s.listen(1)
 conn, addr = s.accept()
