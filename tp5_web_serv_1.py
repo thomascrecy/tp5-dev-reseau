1b7dda25e220afbca853a16caec1e6e630f1c5b0
thomascrecy <126467625+thomascrecy@users.noreply.github.com>
2024-10-24 10:28:41 +0200

    1er serveur

--- tp5_web_serv_1.py
+++ tp5_web_serv_1.py
@@ -1,8 +1,23 @@
 import socket
 
-file = open('index.html')
-html_content = file.read()
-file.close()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('10.4.4.11', 13337))
+sock.listen()
 
-http_response = 'HTTP/1.0 200 OK\n\n' + html_content
-s.send(response.encode())
+while True:
+    client, client_addr = sock.accept()  
+    while True:
+        data = client.recv(1024).decode("utf-8")
+        if not data:
+            break
+
+        response = ""
+        if "GET /" in data:
+            response = "HTTP/1.0 200 OK\n\n<h1>Hello je suis un serveur HTTP</h1>"
+        client.send(response.encode())
+
+        break
+
+client.close()
+s.close()
