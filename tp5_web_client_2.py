c627c2d30986b0221d8d3a77c128b994deb07fd0
thomascrecy <126467625+thomascrecy@users.noreply.github.com>
2024-10-24 10:58:19 +0200

    je tente un truc

--- tp5_web_client_2.py
+++ tp5_web_client_2.py
@@ -5,9 +5,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.4.4.11', 13337))
 s.send('GET /'.encode())
 
-# On reçoit la string Hello
-data = s.recv(1024)
-
 # Réception et affichage du résultat
 s_data = s.recv(1024)
 print(s_data.decode())
