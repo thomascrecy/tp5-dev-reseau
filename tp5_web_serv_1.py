43f884956089d31bbe399b18deed61803a0c01e5
thomascrecy <126467625+thomascrecy@users.noreply.github.com>
2024-10-24 10:30:20 +0200

    1er serveur

--- tp5_web_serv_1.py
+++ tp5_web_serv_1.py
@@ -2,7 +2,7 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('10.4.4.11', 13337))
+sock.bind(("localhost", 13337))
 sock.listen()
 
 while True:
